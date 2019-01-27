a81dc1b5d7222774bf5b3913e53c8945330bc793
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-01-27 22:28:05 +0800

    [add] add gamer model, controller,view

---
+++ NTUHr.Web/Controllers/GamerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using NTUHr.Web.Models;
+
+namespace NTUHr.Web.Controllers
+{
+    public class GamerController : Controller
+    {
+        public ActionResult Details()
+        {
+            var gamer = new Gamer()
+            {
+                Id = 1,
+                Name = "Name1",
+                Gender = "Male",
+                City = "City1"
+            };
+            return View(gamer);
+        }
+    }
+}
---
+++ NTUHr.Web/Models/Gamer.cs
@@ -0,0 +1,10 @@
+﻿namespace NTUHr.Web.Models
+{
+    public class Gamer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}

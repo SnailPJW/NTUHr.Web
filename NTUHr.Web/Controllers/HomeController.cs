758779c8ec9f9c096c6bd552e90da4795898e383
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-01-27 21:21:42 +0800

    加入專案檔案。

---
+++ NTUHr.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace NTUHr.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index(string id, string name)
+        {
+            //string queryString = Request.QueryString["name"];
+            //return $"Hello,{id}, name={queryString}";
+            return $"Hello,{id}, name={name}";
+        }
+        public string GetStringA()
+        {
+            return "Peggy";
+        }
+    }
+}
---
+++ NTUHr.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NTUHr.Web.MvcApplication" Language="C#" %>

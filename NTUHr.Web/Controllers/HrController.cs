bb3bc868bd5c5abe93134c31c88baac73c8820cf
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-02-05 00:18:01 +0800

    [fix] 可以爬完第一頁，第Ｎ頁還在研究

--- NTUHr.Web/Controllers/HrController.cs
+++ NTUHr.Web/Controllers/HrController.cs
@@ -1,5 +1,7 @@
 ﻿using HtmlAgilityPack;
 using NTUHr.Web.Models;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -15,35 +17,72 @@ namespace NTUHr.Web.Controllers
         public ActionResult HrDetails()
         {
             //string htmlUri = "";
-            MemoryStream ms;
-            using (WebClient client = new WebClient())
-            {
-                client.Headers.Add(HttpRequestHeader.UserAgent, "AvoidError");
-                //htmlUri = client.DownloadString("https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx");
-                ms = new MemoryStream(client.DownloadData("https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx"));
-            }
-            HtmlDocument doc = new HtmlDocument();
-            doc.Load(ms, Encoding.UTF8);
-
-            // 裝載第一層查詢結果 
-            HtmlDocument rootDoc = new HtmlDocument();
-
-            //XPath 來解讀它 /html[1]/body[1]/center[1]/table[2]/tr[1]/td[1]/table[1] 
-            rootDoc.LoadHtml(doc.DocumentNode.SelectSingleNode("//table[@id='gvwSel']").InnerHtml);
-
-            var headers = rootDoc.DocumentNode.SelectNodes("tr/th/font");
-
             DataTable dataTable = new DataTable();
-            foreach (HtmlNode header in headers)
-                dataTable.Columns.Add(header.InnerText); // create columns from th
-            // select rows with td elements 
-            foreach (var row in rootDoc.DocumentNode.SelectNodes("(//tr[td])[position()>2 and position()<last()-1]"))
-                dataTable.Rows.Add(row.SelectNodes("td").Select(td => td.InnerText).ToArray());
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx");
+                //索引目前頁面
+                int pageIndex = 1;
+                //找到Post的Container
+                var containerElement = driver.FindElement(By.XPath("//*[@id='gvwSel']/tbody"));
+                //第一個tr為分頁連結
+                var paginationElement = containerElement.FindElements(By.XPath("//tr[1]/td/table/tbody/tr/td"));
+                //表格標頭
+                var tableHeaders = containerElement.FindElements(By.XPath("//tr[2]/th"));
+                foreach (var header in tableHeaders)
+                    dataTable.Columns.Add(header.Text); // create columns from th
+                //表格內容
+                var rootTable = containerElement.FindElements(By.XPath("(tr[td])[position()>1 and position()<last()]"));
+
+                foreach (var page in paginationElement)
+                {
+                    foreach (var row in rootTable)
+                    {
+                        dataTable.Rows.Add(row.FindElements(By.XPath("td")).Select(td => td.Text).ToArray());
+                    }
+                    //換頁
+                    pageIndex++;
+                    var Page = page.FindElements(By.XPath($"td[position()>{pageIndex}]"));
+                    page.Click();
+                }
+            }
 
             ViewBag.DataTable = dataTable;
 
             return View();
         }
+        //public ActionResult HrDetails()
+        //{
+        //    //string htmlUri = "";
+        //    MemoryStream ms;
+        //    using (WebClient client = new WebClient())
+        //    {
+        //        client.Headers.Add(HttpRequestHeader.UserAgent, "AvoidError");
+        //        //htmlUri = client.DownloadString("https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx");
+        //        ms = new MemoryStream(client.DownloadData("https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx"));
+        //    }
+        //    HtmlDocument doc = new HtmlDocument();
+        //    doc.Load(ms, Encoding.UTF8);
+
+        //    // 裝載第一層查詢結果 
+        //    HtmlDocument rootDoc = new HtmlDocument();
+
+        //    //XPath 來解讀它 /html[1]/body[1]/center[1]/table[2]/tr[1]/td[1]/table[1] 
+        //    rootDoc.LoadHtml(doc.DocumentNode.SelectSingleNode("//table[@id='gvwSel']").InnerHtml);
+
+        //    var headers = rootDoc.DocumentNode.SelectNodes("tr/th/font");
+
+        //    DataTable dataTable = new DataTable();
+        //    foreach (HtmlNode header in headers)
+        //        dataTable.Columns.Add(header.InnerText); // create columns from th
+        //    // select rows with td elements 
+        //    foreach (var row in rootDoc.DocumentNode.SelectNodes("(//tr[td])[position()>2 and position()<last()-1]"))
+        //        dataTable.Rows.Add(row.SelectNodes("td").Select(td => td.InnerText).ToArray());
+
+        //    ViewBag.DataTable = dataTable;
+
+        //    return View();
+        //}
         //public ActionResult HrDetails()
         //{
         //    WebClient url = new WebClient();
@@ -80,7 +119,7 @@ namespace NTUHr.Web.Controllers
         //        listHeader.Add(htnode[i].InnerHtml);
         //        htmlNodeCollection[i] = hdc.DocumentNode.SelectNodes(
         //            $"(//td[(count(//tr/th[.='{listHeader[i]}']/preceding-sibling::*)+1)]/font/span)[position()>1 and position()<last()]");
-                
+
         //    }
         //    //for (int j = 0; j < columnPsnSelectNo.Count; j++)
         //    //{

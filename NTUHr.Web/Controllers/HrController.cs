41002f94c89136f6099ed6846c9298b27c9870a7
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-02-05 17:32:30 +0800

    [fix] Refactoring

--- NTUHr.Web/Controllers/HrController.cs
+++ NTUHr.Web/Controllers/HrController.cs
@@ -16,40 +16,63 @@ namespace NTUHr.Web.Controllers
     {
         public ActionResult HrDetails()
         {
-            //string htmlUri = "";
+            ViewBag.DataTable = GetHrDetailsDataTable();
+            return View();
+        }
+        private DataTable GetHrDetailsDataTable()
+        {
             DataTable dataTable = new DataTable();
+            string xpathContainerRoot = "//*[@id='gvwSel']/tbody";
             using (IWebDriver driver = new ChromeDriver())
             {
                 driver.Navigate().GoToUrl("https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx");
-                //索引目前頁面
-                int pageIndex = 1;
                 //找到Post的Container
-                var containerElement = driver.FindElement(By.XPath("//*[@id='gvwSel']/tbody"));
-                //第一個tr為分頁連結
-                var paginationElement = containerElement.FindElements(By.XPath("//tr[1]/td/table/tbody/tr/td"));
-                //表格標頭
-                var tableHeaders = containerElement.FindElements(By.XPath("//tr[2]/th"));
-                foreach (var header in tableHeaders)
-                    dataTable.Columns.Add(header.Text); // create columns from th
-                //表格內容
-                var rootTable = containerElement.FindElements(By.XPath("(tr[td])[position()>1 and position()<last()]"));
-
-                foreach (var page in paginationElement)
-                {
-                    foreach (var row in rootTable)
-                    {
-                        dataTable.Rows.Add(row.FindElements(By.XPath("td")).Select(td => td.Text).ToArray());
-                    }
-                    //換頁
-                    pageIndex++;
-                    var Page = page.FindElements(By.XPath($"td[position()>{pageIndex}]"));
-                    page.Click();
-                }
+                var containerElement = driver.FindElement(By.XPath(xpathContainerRoot));
+                //取得表格標頭
+                GetTableHeaders(driver, ref dataTable);
+                //取得分頁表元素
+                var paginationElement = GetPagination(containerElement);
+                //移動到下一頁
+                Move2NextPage(paginationElement, 2);
+                //重新擷取DOM
+                containerElement = driver.FindElement(By.XPath(xpathContainerRoot));
+                //取得表格內容
+                GetTableRows(containerElement, ref dataTable);
             }
+            return dataTable;
+        }
+        private IWebElement GetPagination(IWebElement iElement)
+        {
+            string xpathPagination = "//tr[1]/td/table/tbody/tr";
+            var paginationElement = iElement.FindElement(By.XPath(xpathPagination));
 
-            ViewBag.DataTable = dataTable;
+            return paginationElement;
+        }
+        private void Move2NextPage(IWebElement iElement, int pageIndex)
+        {
+            var pageN = iElement.FindElement(By.XPath($"//td[{pageIndex}]/a"));
+            pageN.Click();
+        }
+        private void GetTableHeaders(IWebDriver iDriver, ref DataTable dataTable)
+        {
+            string xpathTableHeaders = "//tr[2]/th";
+            //表格標頭
+            var tableHeaders = iDriver.FindElements(By.XPath(xpathTableHeaders));
+            foreach (var header in tableHeaders)
+            {
+                dataTable.Columns.Add(header.Text); // create columns from th
+            }
+        }
+        private void GetTableRows(IWebElement iElement, ref DataTable dataTable)
+        {
+            string xpathTableRows = "(tr[td])[position()>1 and position()<last()]";
+            //表格內容
+            var rootTable = iElement.FindElements(By.XPath(xpathTableRows));
 
-            return View();
+            foreach (var row in rootTable)
+            {
+                dataTable.Rows.Add(row.FindElements(By.XPath("td")).Select(td => td.Text).ToArray());
+            }
         }
         //public ActionResult HrDetails()
         //{
